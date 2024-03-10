e82d09a889d276849981cd2c3a94d9aa7a743e47
Ivan Fonin <ivanfonin@yandex.ru>
2024-03-10 18:54:47 +0300

    feat: Добавить мидлвары helmet и rateLimit

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
 import express from 'express';
 import { config } from 'dotenv';
+import helmet from 'helmet';
+import rateLimit from 'express-rate-limit';
 import database from './database';
 import routes from './routes';
 import { requestLogger, errorLogger } from './middlewares/logger';
@@ -17,6 +19,16 @@ const app = express();
 
 // Настройки Express
 app.use(express.json());
+
+// Настройки безопасности
+app.use(helmet());
+
+// Ограничиваем кол-во запросов
+const limiter = rateLimit({
+  windowMs: 1000 * 60 * 15, // 15 минут
+  max: 100, // максимум 100 запросов с одного IP
+});
+app.use(limiter);
 
 // Логер запросов
 app.use(requestLogger);
