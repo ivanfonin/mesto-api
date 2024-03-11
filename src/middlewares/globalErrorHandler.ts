11e3cb768915cabe8607162717f929370eba12d8
Ivan Fonin <ivanfonin@yandex.ru>
2024-03-11 09:45:47 +0300

    refactor: Возвращать только один ответ сервера из глобального обработчика ошибок

--- src/middlewares/globalErrorHandler.ts
+++ src/middlewares/globalErrorHandler.ts
@@ -3,11 +3,24 @@ import { constants } from 'http2';
 import { isCelebrateError } from 'celebrate';
 import { AppError } from '../errors';
 
+type TErrorResponse = {
+  statusCode: number;
+  message: string;
+  errors?: { [key: string]: string };
+}
+
 const globalErrorHandler = (err: AppError, req: Request, res: Response, next: NextFunction) => {
   const {
     statusCode = constants.HTTP_STATUS_INTERNAL_SERVER_ERROR,
     message,
   } = err;
+
+  const response: TErrorResponse = {
+    statusCode,
+    message: statusCode === constants.HTTP_STATUS_INTERNAL_SERVER_ERROR
+      ? 'На сервере произошла ошибка'
+      : message,
+  };
 
   if (isCelebrateError(err)) {
     const errors: any = {};
@@ -20,16 +33,12 @@ const globalErrorHandler = (err: AppError, req: Request, res: Response, next: Ne
       });
     });
 
-    res
-      .status(constants.HTTP_STATUS_BAD_REQUEST)
-      .send({ message: 'Ошибка валидации', errors });
+    response.statusCode = constants.HTTP_STATUS_BAD_REQUEST;
+    response.message = 'Ошибка валидации';
+    response.errors = errors;
   }
 
-  res.status(statusCode).send({
-    message: statusCode === constants.HTTP_STATUS_INTERNAL_SERVER_ERROR
-      ? 'На сервере произошла ошибка'
-      : message,
-  });
+  res.status(statusCode).send(response);
 
   next();
 };
